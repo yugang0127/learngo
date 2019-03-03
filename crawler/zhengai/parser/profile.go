958cd5399b62570834a70c235152696c5c191949
Yu Gang <yugang@xin.com>
2019-03-03 21:02:52 +0800

    单任务版爬虫完成

--- crawler/zhengai/parser/profile.go
+++ crawler/zhengai/parser/profile.go
@@ -16,28 +16,31 @@ func ParseProfile(contents []byte, name string) engine.ParseResult  {
 	re := regexp.MustCompile(profileRe)
 	matches := re.FindAllSubmatch(contents, -1)
 
-	if len(matches) < 9 {
-		fmt.Println("Profile Parse Error:", len(matches), matches)
-	}
 	for _, m := range matches {
-		fmt.Printf("%s\n", m[1])
+		//fmt.Printf("%s\n", m[1])
+		if strings.Index(string(m[1]), "岁") != -1 {
+			age, _ := strconv.Atoi(strings.Trim(string(m[1]), "岁"))
+			profile.Age = age
+		}
+		if strings.Index(string(m[1]), "cm") != -1 {
+			height, _ := strconv.Atoi(strings.Trim(string(matches[3][1]), "cm"))
+			profile.Height = height
+		}
+		if strings.Index(string(m[1]), "kg") != -1 {
+			weight, _ := strconv.Atoi(strings.Trim(string(matches[4][1]), "kg"))
+			profile.Weight = weight
+		}
+		if strings.Index(string(m[1]), "座(") != -1 {
+			profile.XinZou = string(m[1])
+		}
+		if strings.Index(string(m[1]), "月收入:") != -1 {
+			profile.Income = string(m[1])
+		}
+		if strings.Index(string(m[1]), "工作地:") != -1 {
+			profile.Occupation = string(m[1])
+		}
 	}
-	profile.Marriage = string(matches[0][1])
-	age, _ := strconv.Atoi(strings.Trim(string(matches[1][1]), "岁"))
-	profile.Age = age
-	profile.XinZou = string(matches[2][1])
-	height, _ := strconv.Atoi(strings.Trim(string(matches[3][1]), "cm"))
-	profile.Height = height
-	weight, _ := strconv.Atoi(strings.Trim(string(matches[4][1]), "kg"))
-	profile.Weight = weight
-	profile.Occupation = string(matches[5][1])
-	profile.Income = string(matches[6][1])
-	if len(matches) == 8 {
-		profile.Education = string(matches[7][1])
-	} else {
-		profile.Occupation = string(matches[7][1])
-		profile.Education = string(matches[8][1])
-	}
+
 
 	fmt.Println(profile)
 	result := engine.ParseResult{
